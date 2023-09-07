ba4d4abecb6dd735662f2aab97ce4dea0f143af3
Tibo Melis <tibo.melis@edu-kw1c.nl>
2023-09-07 17:09:55 +0200

    help command setup and templates

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'help', // required! (usually the same as file name)
+    short: '', // if you want it
+    description: 'Show help menu', // usefull, not required
+    category: 'general', // empty for 'misc'
+    disabled: false,
+
+    /**
+     * Executes the command in te command handler
+     * @param {Discord.Client} client   // your bot client
+     * @param {Discord.Message} msg     // the message that triggered this command
+     * @param {Array} args              // any arguments passed trough the command
+     * @param {String} curPrefix        // this bot prefix
+     */
+    async execute(client, msg, args, curPrefix) {
+        const embed = new Discord.EmbedBuilder();
+
+        embed.setTitle('Help - home');
+        embed.setDescription('This is the help menu!');
+        embed.setColor('0xa2db6b');
+
+        msg.channel.send({ embeds: [embed] });
+    },
+};
