acff11ae6d159db78e89d48ae9960a03b881c0b6
Tibo Melis <tibo.melis@edu-kw1c.nl>
2024-04-03 11:12:10 +0200

    shakespear test

---
+++ interactions/slashcommands/shakespear.js
@@ -0,0 +1,42 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    EmbedBuilder,
+} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shakespear')
+        .setDescription('Translate a sentance to shakespearian text.')
+        .addStringOption((option) =>
+            option
+                .setName('text')
+                .setDescription('The text to translate.')
+                .setRequired(true)
+        ),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction) {
+        interaction.deferReply();
+        const text = interaction.options.getString('text');
+        const r = await fetch(
+            `https://api.funtranslations.com/translate/shakespeare.json?text=${text}`
+        );
+        const x = await r.json();
+
+        const translated = x?.contents?.translated;
+
+        const embed = new EmbedBuilder();
+        embed.setName('Hither is thy translation!');
+        embed.setDescription(`translation: ${translated}`);
+        embed.setColor('#ff0000');
+
+        interaction.editReply({ embeds: [embed] });
+    },
+};
+
+async function translate(text) {
+    return x.contents.translated;
+}
